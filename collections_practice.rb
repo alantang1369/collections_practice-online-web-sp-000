8aa4403978f098170bb4f8071b318a92a51843c3
Alan Tang <giabao_tang@yahoo.com>
2020-09-27 04:34:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,4 +7,14 @@ def sort_array_desc(array)
     b <=> a 
   end
 end
- def 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0 
+    elsif a.length < b.length 
+      -1 
+    elsif a.length > b.length 
+      1
+    end  
+end 
